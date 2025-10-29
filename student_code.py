e2b999afc6d114e7281be5ed598151c8d9f572c5
jit325 <jit325@lehigh.edu>
2025-10-29 15:24:53 -0400

    Update student_code.py

--- student_code.py
+++ student_code.py
@@ -4,17 +4,29 @@ class SortableDigraph:
     """Base class providing basic graph structure and topological sort"""
     def __init__(self):
         self.adj = {}  # adjacency list
+        self.node_data = {}  # store node data
+        self.edge_weights = {}  # store edge weights
         
     def add_node(self, node, data=None):
         """Add a node to the graph"""
         if node not in self.adj:
             self.adj[node] = []
+            self.node_data[node] = data
     
     def add_edge(self, u, v, edge_weight=None):
         """Add an edge from u to v"""
         self.add_node(u)
         self.add_node(v)
         self.adj[u].append(v)
+        self.edge_weights[(u, v)] = edge_weight
+    
+    def get_node_value(self, node):
+        """Get the data associated with a node"""
+        return self.node_data.get(node)
+    
+    def get_edge_weight(self, u, v):
+        """Get the weight of an edge"""
+        return self.edge_weights.get((u, v))
     
     def topsort(self):
         """Topological sort"""
@@ -45,8 +57,8 @@ class SortableDigraph:
     def predecessors(self, node):
         """Get all predecessors of a node"""
         preds = []
-        for u in self.adj:
-            if node in self.adj[u]:
+        for u, neighbors in self.adj.items():
+            if node in neighbors:
                 preds.append(u)
         return preds
     
